df022f787718ddc8c3024c7cdc4b0f45695df31b
Rawoosa <rawoosa@protonmail.com>
2023-10-23 23:43:30 +0300

    Added mod files from a mod template.

---
+++ src/client/java/rawoosa/gunmod/RawoosasGunModClient.java
@@ -0,0 +1,10 @@
+package rawoosa.gunmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RawoosasGunModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
