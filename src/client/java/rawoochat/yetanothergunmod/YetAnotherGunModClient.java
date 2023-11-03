93e1401450d51f1b26ca029e55547aac86d8c7f1
valeriigurskiy <valeriygurskiy7@gmail.com>
2023-11-03 23:58:03 +0200

    test comit

--- src/client/java/rawoochat/yetanothergunmod/YetAnotherGunModClient.java
+++ src/client/java/rawoochat/yetanothergunmod/YetAnotherGunModClient.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ClientModInitializer;
 public class YetAnotherGunModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		// test
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
